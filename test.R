571758cfecc7886db94cdc5129c5d37ef4f99f46
gitcris <pipapeiler@web.de>
2017-02-21 11:15:17 +0100

    Add old functions to test.R

---
+++ test.R
@@ -0,0 +1,19 @@
+# Click "Submit" button -> save data
+# observeEvent(input$BUTyes, {
+#   toggleModal(session, "confirm")#, toggle = "close")
+#   if (input$id != "0") {
+#     UpdateData(formData())
+#   } else {
+#     CreateData(formData())
+#     # Updates the editing fields below datatable
+#     UpdateInputs(CreateDefaultRecord(), session)
+#   }
+# }, priority = 1)
+
+# Press "Delete" button -> delete from data
+# observeEvent(input$delete, {
+#   DeleteData(formData())
+#   # Updates the editing fields below datatable
+#   UpdateInputs(CreateDefaultRecord(), session)
+# }, priority = 1)
+
